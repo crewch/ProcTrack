0e7335d565a01b7053384f1f941ddeda97e94dec
Dzheadi2376 <zdv376@gmail.com>
2023-07-03 18:11:20 +0300

    fix

--- frontend/src/components/MainPage/ContainerListProcess/ContainerListProcess.tsx
+++ frontend/src/components/MainPage/ContainerListProcess/ContainerListProcess.tsx
@@ -6,6 +6,7 @@ import DataDialog from './DataDialog/DataDialog'
 import styles from '/src/styles/MainPageStyles/ContainerListProcessStyles/ContainerListProcess.module.scss'
 import SettingsList from './Search/SettingsList'
 import DataTable from '../Dialogs/DataTable/DataTable'
+import AddProcessButton from '../SelectedProcess/StagesList/AddProcessButton/AddProcessButton'
 
 const ContainerListProcess = () => {
 	const [textForSearchProcess, setTextForSearchProcess] = useState('')
@@ -25,9 +26,12 @@ const ContainerListProcess = () => {
 			) : (
 				<>
 					<ListProccess />
-					<DataDialog title='Tабличное представление' icon='table'>
-						<DataTable />
-					</DataDialog>
+					<Box className={styles.btns}>
+						<AddProcessButton />
+						<DataDialog title='Tабличное представление' icon='table'>
+							<DataTable />
+						</DataDialog>
+					</Box>
 				</>
 			)}
 		</Box>
