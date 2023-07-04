1bca32d2011b9918430588e81be99d97311c1ff8
Dzheadi2376 <zdv376@gmail.com>
2023-07-04 14:48:47 +0300

    fiz

--- frontend/src/components/MainPage/Dialogs/DataGraph/DataGraph.tsx
+++ frontend/src/components/MainPage/Dialogs/DataGraph/DataGraph.tsx
@@ -7,11 +7,19 @@ import ReactFlow, {
 	addEdge,
 	Edge,
 	Connection,
+	Panel,
 } from 'reactflow'
 import 'reactflow/dist/style.css'
+import dagre from 'dagre'
+import { Button } from '@mui/material'
 
 const acceptanceColor = '#84DE02'
 const rejectionColor = '#E32636'
+
+const initialEdges = [
+	{ id: 'e1-2', source: '1', target: '2' },
+	{ id: 'e1-3', source: '1', target: '3', animated: true },
+]
 
 const initialNodes = [
 	{
@@ -42,18 +50,63 @@ const initialNodes = [
 		},
 	},
 ]
-const initialEdges = [
-	{ id: 'e1-2', source: '1', target: '2' },
-	{ id: 'e1-3', source: '1', target: '3', animated: true },
-]
 
-function DataGraph() {
+const dagreGraph = new dagre.graphlib.Graph()
+dagreGraph.setDefaultEdgeLabel(() => ({}))
+
+const nodeWidth = 172
+const nodeHeight = 36
+
+const getLayoutedElements = (nodes: any[], edges: any[], direction = 'TB') => {
+	const isHorizontal = direction === 'LR'
+	dagreGraph.setGraph({ rankdir: direction })
+
+	nodes.forEach(node => {
+		dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight })
+	})
+
+	edges.forEach(edge => {
+		dagreGraph.setEdge(edge.source, edge.target)
+	})
+
+	dagre.layout(dagreGraph)
+
+	nodes.forEach(node => {
+		const nodeWithPosition = dagreGraph.node(node.id)
+		node.targetPosition = isHorizontal ? 'left' : 'top'
+		node.sourcePosition = isHorizontal ? 'right' : 'bottom'
+
+		node.position = {
+			x: nodeWithPosition.x - nodeWidth / 2,
+			y: nodeWithPosition.y - nodeHeight / 2,
+		}
+
+		return node
+	})
+
+	return { nodes, edges }
+}
+
+getLayoutedElements(initialNodes, initialEdges)
+
+const DataGraph = () => {
 	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
 	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
 
 	const onConnect = useCallback(
 		(params: Edge | Connection) => setEdges(eds => addEdge(params, eds)),
 		[setEdges]
+	)
+
+	const onLayout = useCallback(
+		(direction: string | undefined) => {
+			const { nodes: layoutedNodes, edges: layoutedEdges } =
+				getLayoutedElements(nodes, edges, direction)
+
+			setNodes([...layoutedNodes])
+			setEdges([...layoutedEdges])
+		},
+		[nodes, edges, setNodes, setEdges]
 	)
 
 	return (
@@ -70,6 +123,10 @@ function DataGraph() {
 				onEdgesChange={onEdgesChange}
 				onConnect={onConnect}
 			>
+				<Panel position='top-right'>
+					<Button onClick={() => onLayout('TB')}>Вертикальный вид</Button>
+					<Button onClick={() => onLayout('LR')}>Горизонтальный вид</Button>
+				</Panel>
 				<Controls />
 				<Background gap={12} size={1} />
 			</ReactFlow>
