36e685fd09115e3b27192dc1b634562559ba4daa
Uzym <lyutoev2003ia@gmail.com>
2023-07-05 14:42:10 +0300

    update db

---
+++ DB-Service/Migrations/20230705114114_create17.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DB_Service.Migrations
+{
+    public partial class create17 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "Passports",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Passports");
+        }
+    }
+}
