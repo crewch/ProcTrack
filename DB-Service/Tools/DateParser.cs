de8fb61ff2ca9bcae7a964297a90b7fca96aa998
Pavloffff <pavlov.id.2003@gmail.com>
2023-07-09 01:18:54 +0300

    parser fix

--- DB-Service/Tools/DateParser.cs
+++ DB-Service/Tools/DateParser.cs
@@ -21,19 +21,16 @@
                 "124124"
             };
 
-            List<string> days = new List<string>()
-            {
-                "пн",
-                "вт",
-                "ср",
-                "чт",
-                "пт",
-                "сб",
-                "вс",
-                "12412"
-            };
+            Dictionary<string, string> days = new Dictionary<string, string>();
+            days["Monday"] = "пн";
+            days["Tuesday"] = "вт";
+            days["Wednesday"] = "ср";
+            days["Thursday"] = "чт";
+            days["Friday"] = "пт";
+            days["Saturday"] = "сб";
+            days["Sunday"] = "вс";
 
-            var dayWeek = days[(int)input.DayOfWeek - 1];
+            var dayWeek = days[input.DayOfWeek.ToString()];
             var year = input.Year;
             var month = months[input.Month - 1];
             var day = input.Day;
