c5dee1b8a894450632470dbf2dafd71b28ed76c8
Pavloffff <pavlov.id.2003@gmail.com>
2023-08-17 19:22:50 +0300

    30 min

--- AuthService/Services/LoginService.cs
+++ AuthService/Services/LoginService.cs
@@ -131,7 +131,7 @@ namespace AuthService.Services
             var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
                 _configuration["Jwt:Audience"],
                 claims.ToArray(),
-                expires: DateTime.Now.AddMinutes(1),
+                expires: DateTime.Now.AddMinutes(30),
                 signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
