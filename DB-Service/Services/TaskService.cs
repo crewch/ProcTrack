171b2a76871484f3774bc0c0edaed4353afc197c
Pavloffff <pavlov.id.2003@gmail.com>
2023-08-23 15:31:22 +0300

    fix

--- DB-Service/Services/TaskService.cs
+++ DB-Service/Services/TaskService.cs
@@ -293,9 +293,7 @@ namespace DB_Service.Services
 
 			var stage = await _context.Stages			//NOTE:notification
 				.Include(s => s.Status)
-				.Where(s => s.Id == taskModel.StageId && 
-                       s.Status.Title.ToLower() != "не начат" &&
-                       s.Status.Title.ToLower() != "остановлен")
+				.Where(s => s.Id == taskModel.StageId)
 				.FirstOrDefaultAsync();
 
 			var processForNotification = await _context.Processes
@@ -388,9 +386,7 @@ namespace DB_Service.Services
 
 			var stage = await _context.Stages		//NOTE:notification
 				.Include(s => s.Status)
-				.Where(s => s.Id == taskModel.StageId && 
-                       s.Status.Title.ToLower() != "не начат" &&
-                       s.Status.Title.ToLower() != "остановлен")
+				.Where(s => s.Id == taskModel.StageId)
 				.FirstOrDefaultAsync();
 
 			var status = await _context.Statuses
@@ -475,9 +471,7 @@ namespace DB_Service.Services
 
 			var stage = await _context.Stages		//NOTE:notification
 				.Include(s => s.Status)
-				.Where(s => s.Id == taskModel.StageId && 
-                       s.Status.Title.ToLower() != "не начат" &&
-                       s.Status.Title.ToLower() != "остановлен")
+				.Where(s => s.Id == taskModel.StageId)
 				.FirstOrDefaultAsync();
 
 			var processForNotification = await _context.Processes
